b0aeb3dab297ef6a6cad97c55a5bb2b418c01c4a
Arnaud Watlet <arnaud.watlet@umons.ac.be>
2023-06-07 12:33:32 +0000

    Adds rs_check example

---
+++ examples/rs_check.py
@@ -0,0 +1,15 @@
+from ohmpi import OhmPi
+import matplotlib.pyplot as plt
+import numpy as np
+
+a = np.arange(13) + 1
+b = a + 3
+m = a + 1
+n = a + 2
+seq = np.c_[a, b, m, n]
+
+k = OhmPi(idps=True, use_mux=True)
+k.settings['injection_duration'] = .5
+k.sequence = seq
+
+k.rs_check(tx_volt=5)
